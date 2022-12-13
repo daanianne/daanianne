3463b4e02c12b06add7b0da26a93650f33a446b0
Stu Wilcox <stuart_wilcox@outlook.com>
2022-12-13 11:23:13 -0800

    ready for deployment

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,10 @@
 // this should match the package.json homepage field with '/images' on the end
 const HOST = 'https://daanianne.github.io';
-const BASE_PATH = '';
+const BASE_PATH = 'daanianne';
 const HOMEPAGE_PREFIX = `${HOST}/${BASE_PATH}`;
 
 export const getImageUrl = (imageName: string) => {
-    return `${HOST}/images/${imageName}`;
+    return `${HOMEPAGE_PREFIX}/images/${imageName}`;
 };
 
 export const getLinkUrl = (pageName: string) => {
