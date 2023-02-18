0b4aa0d8d4dec854f257935856595d143a59925b
Stu Wilcox <stuart_wilcox@outlook.com>
2023-02-18 11:21:31 -0800

    update resume link

--- src/components/utils.tsx
+++ src/components/utils.tsx
@@ -1,6 +1,6 @@
 export const downloadResume = () => {
     const resumeAnchor = document.createElement('a');
-    resumeAnchor.setAttribute('href', 'https://github.com/Stuart-Wilcox/Danielle-Humilde-Website/raw/gh-pages/resume/Danielle_Humilde_Resume.pdf');
+    resumeAnchor.setAttribute('href', 'https://github.com/daanianne/daanianne/raw/gh-pages/resume/Danielle_Humilde_Resume.pdf');
     resumeAnchor.setAttribute('download', 'Danielle_Humilde_Resume.pdf');
     document.body.appendChild(resumeAnchor);
     resumeAnchor.click();
